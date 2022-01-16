19da8d0f22b01591fdb18f6fca28329a30139592
davidleongz <leongonzalezdavid@gmail.com>
2022-01-16 21:19:56 +0100

    OrElse OrElseGet demostration

---
+++ src/main/java/com/david/optional/demo/OrElseAndOrElseGetDemo.java
@@ -0,0 +1,51 @@
+package com.david.optional.demo;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class OrElseAndOrElseGetDemo {
+
+    public static String recoverMessageFromSource() {
+
+        try {
+
+            System.out.println("Requesting to source, this could take a while...");
+
+            TimeUnit.SECONDS.sleep(5);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "We need to improve our efficiency!";
+    }
+
+    private static void printIfAny(String veryImportantMessage) {
+        Optional.of(veryImportantMessage)
+                .ifPresent(msj -> {
+                    System.out.println("Finally! what you should know is: " + msj);
+                });
+    }
+
+    public static void main(String[] args) {
+
+        String message = "Do only what you need to do and do it only when you need to";
+
+        String veryImportantMessage = Optional.ofNullable(message)
+                .filter(text -> !text.isEmpty())
+                .map(String::toUpperCase)
+                .orElse(recoverMessageFromSource());
+
+        printIfAny(veryImportantMessage);
+
+        message = "Make It Better!";
+
+        veryImportantMessage = Optional.ofNullable(message)
+                .filter(text -> !text.isEmpty())
+                .map(String::toUpperCase)
+                .orElseGet(OrElseAndOrElseGetDemo::recoverMessageFromSource);
+
+        printIfAny(veryImportantMessage);
+
+    }
+}
